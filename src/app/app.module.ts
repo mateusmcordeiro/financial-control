a45d76e2bfa69f3318177f02fba1f81aabe78d56
Mateus Cordeiro <mateusmcordeiro@hotmail.com>
2020-12-06 17:07:36 -0300

    call componentes on app main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderModule } from 'src/core/header/header.module';
+import { FooterModule } from 'src/core/footer/footer.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
